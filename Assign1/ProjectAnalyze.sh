dc207b4992a19e29bbf1f923f5b57d628f3d886c
Kevin Zhou <35268521+zhouh46@users.noreply.github.com>
2018-02-26 21:27:48 -0500

    Fixed the Last Part

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -15,22 +15,22 @@ fi
 # End #
 
 
-# 2. Puts all uncommited changes in a file changes.log #
+# 2. Puts all uncommitted changes in a file changes.log #
 
 git diff >> changes.log 
-
+echo "All uncommitted changes have been put into changes.log"
 # End #
 
 # 3. Puts each line from every file of your project with the tag #TODO into a file todo.log #
 
 grep -r --exclude=todo.log "#TODO" . >> todo.log
-
+echo "Lines with #TODO have been put into todo.log"
 # End #
 
 # 4. Checks all haskell files for syntax errors and puts the results into error.log #
 shopt -s nullglob
 find. -type f -name "*.hs" -exec ghc -fno-code {} \; &> error.log
-
+echo "All syntax errors have been put into error.log"
 # End #
 
 # Extra: Ask if a git pull is needed #
